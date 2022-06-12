dffb4594123608bb3a72a4a721c8df008a8ef0ef
Ainsdev <ainsn1654@gmail.com>
2022-06-12 09:55:35 -0400

    testing

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 'use strict'
-//variables
+//functionallity for Images Slider
 const previous = document.getElementById('previous');
 const next = document.getElementById('next');
 const carousel = document.getElementById('carousel');
 let counter = 1;
-//functionallity for buttons
 const nextSlide = (btn) => {
 
     btn.addEventListener('click', () => {
@@ -31,3 +30,21 @@ const prevSlide = (btn) => {
 }
 nextSlide(next);
 prevSlide(previous);
+//functionallity for Images Slider
+//
+//functionallity for mobile menu    
+const menu = document.getElementById('mobile_menu');
+const menuBtn = document.getElementById('btn_mobile_menu');
+const closeBtn = document.getElementById('btn_close_mobile_menu');
+//Open Menu
+menuBtn.addEventListener('click', () => {
+    menu.classList.contains('hidden') ? menu.classList.remove('hidden') : menu.classList.add('hidden');
+});
+//Close Menu
+closeBtn.addEventListener('click', () => {
+    if (menu.classList.contains('hidden') == false) {
+        return menu.classList.add('hidden');
+    }
+});
+//functionallity for mobile menu
+
