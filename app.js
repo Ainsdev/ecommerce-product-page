3d9985ca3b5b4c21bf61fc1021e3ca99a3d2d58e
Ainsdev <ainsn1654@gmail.com>
2022-06-12 22:53:00 -0400

    testing

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ prevSlide(previous);
 const menu = document.getElementById('mobile_menu');
 const menuBtn = document.getElementById('btn_mobile_menu');
 const closeBtn = document.getElementById('btn_close_mobile_menu');
+const closeArea = document.getElementById('transparent');
 //Open Menu
 menuBtn.addEventListener('click', () => {
     menu.classList.contains('hidden') ? menu.classList.remove('hidden') : menu.classList.add('hidden');
@@ -46,5 +47,40 @@ closeBtn.addEventListener('click', () => {
         return menu.classList.add('hidden');
     }
 });
+closeArea.addEventListener('click', () => {
+    if (menu.classList.contains('hidden') == false) {
+        return menu.classList.add('hidden');
+    }
+});
 //functionallity for mobile menu
+//  
+//functionallity of Cart UI
+const cart = document.getElementById('cart');
+const cartbtn = document.getElementById('cartbtn');
+const closeCartBtn = document.getElementById('cart_bg');
+function removeHidden() {
+    return {
+        first: cart.classList.remove('hidden'),
+        second: closeCartBtn.classList.remove('hidden'),
+    };
+}
+function hidden() {
+    return {
+        first: cart.classList.add('hidden'),
+        second: closeCartBtn.classList.add('hidden'),
+    };
+}
+
+cartbtn.addEventListener('click', () => {
+    if (cart.classList.contains('hidden') == true) {
+        return removeHidden();
+    }
+});
+closeCartBtn.addEventListener('click', () => {
+    if (cart.classList.contains('hidden') == false) {
+        return hidden();
+    }
+});
+// functionallity of Cart UI
+
 
