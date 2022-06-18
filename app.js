a86621ff9507a830bb03460677355d693bac0825
Ainsdev <ainsn1654@gmail.com>
2022-06-17 21:47:50 -0400

    testing

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const next = document.getElementById('next');
 const carousel = document.getElementById('carousel');
 let counter = 1;
 const nextSlide = (btn) => {
-
     btn.addEventListener('click', () => {
         if (counter < 4) {
             carousel.classList.add(`translate-x-[${-counter * 50}%]`);
@@ -31,6 +30,34 @@ const prevSlide = (btn) => {
 nextSlide(next);
 prevSlide(previous);
 //functionallity for Images Slider
+//
+//functionallity for Images Slider DESKTOP
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const img3 = document.getElementById('img3');
+const img4 = document.getElementById('img4');
+const masterImg = document.getElementById('masterimg');
+const changeImg = (num, imgn) => {
+    masterImg.src = `images/image-product-${num}.jpg`;
+    img1.classList.add('opacity-40');
+    img2.classList.add('opacity-40');
+    img3.classList.add('opacity-40');
+    img4.classList.add('opacity-40');
+    imgn.classList.remove('opacity-40');
+};
+img1.addEventListener('click', () => {
+    changeImg(1, img1);
+});
+img2.addEventListener('click', () => {
+    changeImg(2, img2);
+});
+img3.addEventListener('click', () => {
+    changeImg(3, img3);
+});
+img4.addEventListener('click', () => {
+    changeImg(4, img4);
+});
+//functionallity for Images Slider DESKTOP
 //
 //functionallity for mobile menu    
 const menu = document.getElementById('mobile_menu');
